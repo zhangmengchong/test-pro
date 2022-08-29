a488e458ab7d91adfb5aade4c16bf9fd70c940a2
itzmc <zmc@163.com>
2022-08-29 11:33:11 +0800

    8-29-04

--- src/main/java/Ztest.java
+++ src/main/java/Ztest.java
@@ -5,20 +5,9 @@ import java.util.stream.Collectors;
 public class Ztest {
     public static void main(String[] args) {
         Stack<Integer> stack = new Stack<>();
+        System.out.println("1111");
     }
 
-
-    public static int[] getNext(int[] next, String s) {
-        next[0] = 0;
-        int j = 0;
-        for (int i = 1; i < s.length(); i++) {
-
-            while (s.charAt(i) != s.charAt(j) && j > 0) j = next[j - 1];
-            if (s.charAt(i) == s.charAt(j)) j++;
-            next[i] = j;
-        }
-        return next;
-    }
 
 
 }
